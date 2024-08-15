e9aff24375c90dc8962883b8d18ffb02fb9e5d6c
PtsDrw <77636993+PtsDrw@users.noreply.github.com>
2024-08-15 12:15:14 +0300

    Update codegen.js

--- js/codegen.js
+++ js/codegen.js
@@ -8,23 +8,27 @@ document.getElementById('startBtn').addEventListener('click',
         const generatedKeys = document.getElementById('generatedKeys');
         const keyCount = parseInt(document.getElementById('keyCountSelect').value);
         const gameId = parseInt(document.getElementById('gameSelect').value);
-        if (gameId === 1) {
-            APP_TOKEN = 'd28721be-fd2d-4b45-869e-9f253b554e50';
-            PROMO_ID = '43e35910-c168-4634-ad4f-52fd764a843f';
-            EVENTS_DELAY = 20000;
-        } else if (gameId === 2) {
-            APP_TOKEN = 'd1690a07-3780-4068-810f-9b5bbf2931b2';
-            PROMO_ID = 'b4170868-cef0-424f-8eb9-be0622e8e8e3';
-            EVENTS_DELAY = 20000;
+        if (gameId === 5) {
+            APP_TOKEN = '8d1cc2ad-e097-4b86-90ef-7a27e19fb833';
+            PROMO_ID = 'dc128d28-c45b-411c-98ff-ac7726fbaea4';
+            EVENTS_DELAY = 120000;
+        } else if (gameId === 4) {
+            APP_TOKEN = '82647f43-3f87-402d-88dd-09a90025313f';
+            PROMO_ID = 'c4480ac7-e178-4973-8061-9ed5b2e17954';
+            EVENTS_DELAY = 120000;
         } else if (gameId === 3) {
             APP_TOKEN = '74ee0b5b-775e-4bee-974f-63e7f4d5bacb';
             PROMO_ID = 'fe693b26-b342-4159-8808-15e3ff7f8767';
             EVENTS_DELAY = 120000;
+        } else if (gameId === 2) {
+            APP_TOKEN = 'd1690a07-3780-4068-810f-9b5bbf2931b2';
+            PROMO_ID = 'b4170868-cef0-424f-8eb9-be0622e8e8e3';
+            EVENTS_DELAY = 20000;
         } else {
-            if (gameId === 4) {
-                APP_TOKEN = '82647f43-3f87-402d-88dd-09a90025313f';
-                PROMO_ID = 'c4480ac7-e178-4973-8061-9ed5b2e17954';
-                EVENTS_DELAY = 120000;
+            if (gameId === 1) {
+                APP_TOKEN = 'd28721be-fd2d-4b45-869e-9f253b554e50';
+                PROMO_ID = '43e35910-c168-4634-ad4f-52fd764a843f';
+                EVENTS_DELAY = 20000;
             }
         }
         // Reset UI for new generation
