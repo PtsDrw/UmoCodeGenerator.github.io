a409fc43f7d3071f54f567f3740989ba5b66fa46
PtsDrw <77636993+PtsDrw@users.noreply.github.com>
2024-08-23 15:08:53 +0300

    Update codegen.js

--- js/codegen.js
+++ js/codegen.js
@@ -36,12 +36,24 @@ const gamePromoConfigs = {
         eventsDelay: 20000,
         attemptsNumber: 10
     },
-    PolySphere: {
+    Polysphere: {
         appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         eventsDelay: 20000,
-        attemptsNumber: 22
+        attemptsNumber: 20
     },
+    MowandTrim: {
+        appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        eventsDelay: 20000,
+        attemptsNumber: 20
+    },
+    MudRacing: {
+        appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
+        promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
+        eventsDelay: 20000,
+        attemptsNumber: 20
+    }
 };
 
 
